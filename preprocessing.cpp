ff90f685f363bcd431c2cbb64577181844324d6c
GabrieleGiannessi <gabrielegiannessi@gmail.com>
2026-09-01 15:12:04 +0200

    feat: add preprocessing

---
+++ preprocessing.cpp
@@ -0,0 +1,14 @@
+/**
+ * Codice sorgente per il caricamento e il preprocessing delle immagini CT relative ai pazienti. 
+ * Le immagini sono prelevate dal CORT dataset. (https://doi.org/10.1186/2047-217X-3-37)
+ */
+#include <vector>
+#include <string>
+#define PATIENTS_DIR "./patients/"
+
+
+// Restituisce un vettore di stringhe contenente i dati del paziente
+std::vector<std::string> getPatientData(const std::string& patientId) {
+    
+    return {}; 
+}
