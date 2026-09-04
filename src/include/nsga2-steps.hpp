f00fee36e04a6800eba844f347e2f6454d6151a3
GabrieleGiannessi <gabrielegiannessi@gmail.com>
2026-09-04 12:32:36 +0200

    feat: add header file for NSGA-II algorithm functions

---
+++ src/include/nsga2-steps.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file nsga2-steps.hpp
+ * @brief Operazioni timerizzate componibili dell'algoritmo NSGA-II.
+ */
+#pragma once
+
+#include "evaluator.hpp"
+#include "population.hpp"
+
+#include <random>
+#include <vector>
+
+void evaluatePopulation(Population &population, Evaluator &evaluator);
+
+std::vector<std::vector<int>> sortPopulation(Population &population);
+
+void assignPopulationCrowding(
+    Population &population, const std::vector<std::vector<int>> &fronts);
+
+Population generatePopulationOffspring(
+    Population &population, double crossover_probability,
+    double mutation_probability, double eta_c, double eta_m,
+    std::mt19937 &rng);
+
+Population mergePopulations(const Population &population,
+                            const Population &offspring);
+
+Population truncatePopulationByFronts(
+    Population &population, const std::vector<std::vector<int>> &fronts,
+    int population_size);
